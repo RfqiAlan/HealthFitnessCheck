5e6c48eda3ce9b1c6b40489e75e7d5998adb7fdc
Rai-14 <sitarasmirasmi@gmail.com>
2024-10-10 00:39:08 -0700

    Modul4.py

--- Modul4.py
+++ Modul4.py
@@ -3,4 +3,5 @@ def rekomendasi_asupan_air(berat_badan):
         return berat_badan * 35 / 1000
     else:
         return "Masukkan dalam bentuk angka"
-print(rekomendasi_asupan_air("A"))
+berat = rekomendasi_asupan_air(50)
+print(f"{berat} liter")
