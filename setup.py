2bb5579d28a309e851f4d289e769b18fd07bb582
RfqiAlan <rifqialan46475@gmail.com>
2024-10-10 21:52:05 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HealthFitness',
-    version='0.1.2',
+    version='0.1.3',
     description='Menyediakan fitur pengecekan kesehatan dan fitness',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
